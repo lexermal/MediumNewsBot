fb7b0ced4143df4de1678a61f2debe1afcb3c828
lexermal <contact@weixler.me>
2021-12-10 13:09:31 +0100

    Added error logging for fetcher

--- src/Fetcher.ts
+++ src/Fetcher.ts
@@ -1,8 +1,14 @@
 import Parser from "rss-parser";
 import { Article } from "./entity/Article";
 import { Source, SourceType } from "./entity/Source";
+import Log from "./Logger";
 
 export class Fetcher {
+    private logger: Log;
+
+    constructor(logger?: Log) {
+        this.logger = logger || Log.getInstance();
+    }
 
     public async getLatestArticles(source: Source) {
         return await this.getCurrentArticles(this.getURL(source.type, source.urlParts));
@@ -17,7 +23,7 @@ export class Fetcher {
             case SourceType.TAG:
                 return `https://medium.com/feed/tag/${urlParts[0]}`;
             // case SourceType.TAGGED:
-                // return `https://medium.com/feed/${urlParts[0]}/tagged/${urlParts[1]}`;
+            // return `https://medium.com/feed/${urlParts[0]}/tagged/${urlParts[1]}`;
             case SourceType.PUBLICATION:
                 return `https://medium.com/feed/${urlParts[0]}`;
             default:
@@ -29,21 +35,26 @@ export class Fetcher {
     async getCurrentArticles(url: string): Promise<Article[]> {
         const parser = new Parser();
 
-        const feed = await parser.parseURL(url);
-        const posts = feed.items.filter(item => typeof item.categories !== 'undefined' && item.categories.length > 0)
+        return parser.parseURL(url)
+            .catch(e => {
+                this.logger.error(`The following error accured when fetching articles from ${url}: ${e.message}`, e);
+                return { items: [] };
+            }).then(feed => {
+                const posts = feed.items.filter(item => typeof item.categories !== 'undefined' && item.categories.length > 0)
 
-        return posts.map(item => {
-            const article = new Article();
+                return posts.map(item => {
+                    const article = new Article();
 
-            article.articleId = item.guid!;
-            article.title = item.title!;
-            article.link = item.link!.split("?")[0];
-            article.creator = item.creator!;
-            article.pubDate = item.isoDate!;
-            article.previewText = item.contentSnippet||"";
-            article.setCategories(item.categories!);
+                    article.articleId = item.guid!;
+                    article.title = item.title!;
+                    article.link = item.link!.split("?")[0];
+                    article.creator = item.creator!;
+                    article.pubDate = item.isoDate!;
+                    article.previewText = item.contentSnippet || "";
+                    article.setCategories(item.categories!);
 
-            return article;
-        })
+                    return article;
+                })
+            })
     }
 }
