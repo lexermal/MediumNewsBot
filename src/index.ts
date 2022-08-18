c550f1fb31d8c76ffc8d800dafc0b1ecceb816f6
lexermal <contact@weixler.me>
2022-08-18 13:14:45 +0200

    Improved structure of index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,19 @@
 import "reflect-metadata";
-import { DataSource } from 'typeorm';
 import BotController from "./controller/BotController";
 import DatabaseController from "./controller/DatabaseController";
-import { attachBlacklistHandling } from "./_old/bot/Blacklist";
-import { attachSourceHandling } from "./_old/bot/Source";
+import { attachBlacklistHandling as attachBlacklistListeners } from "./_old/bot/Blacklist";
+import { attachSourceHandling as attachSourceListeners } from "./_old/bot/Source";
 import { Content } from "./_old/content/Content";
 import { fetchNewArticles } from "./_old/utils/ArticleFetcher";
 import { sendNewArticles } from "./_old/utils/ArticleSender";
 import Log from "./_old/utils/Logger";
 
-const bot = BotController.getBot();
 
-async function startBot(con: DataSource) {
-    attachSourceHandling();
-    attachBlacklistHandling(con);
+Log.info("Starting Medium News bot.");
+
+DatabaseController.initDB().then(() => {
+    attachSourceListeners();
+    attachBlacklistListeners();
 
     BotController.setWelcomeMessage(() => Content.start);
 
@@ -21,16 +21,8 @@ async function startBot(con: DataSource) {
 
     BotController.launch();
 
-    Log.info("Successfully started the telegram bot!");
-    return con;
-}
+    Log.info("Successfully started Medium News bot!");
 
-
-Log.info("Starting the telegram bot.");
-
-DatabaseController.initDB().then(() => {
-    return DatabaseController.getConnection();
-}).then(startBot).then(async con => {
 
     Log.info("Starting to fetch new articles and send out unread ones.");
 
@@ -38,6 +30,10 @@ DatabaseController.initDB().then(() => {
     const sendingDuration = Number(process.env.SENDING_DURATION || 6); //minutes
 
     fetchNewArticles(fetchingDuration);
+
+    const con = DatabaseController.getConnection();
+    const bot = BotController.getBot();
+
     sendNewArticles(bot, con, sendingDuration);
 }).catch(e => {
     Log.error(e.message, e);
